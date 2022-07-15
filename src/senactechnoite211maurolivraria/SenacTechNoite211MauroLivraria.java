a52556d453015940b6832d9bd7e88528618f5076
Mauro Benck Homrich Junior <182110041@senacinformatica.br>
2022-07-15 19:34:12 -0300

    Criando o projeto

---
+++ src/senactechnoite211maurolivraria/SenacTechNoite211MauroLivraria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package senactechnoite211maurolivraria;
+
+/**
+ *
+ * @author 182110041
+ */
+public class SenacTechNoite211MauroLivraria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

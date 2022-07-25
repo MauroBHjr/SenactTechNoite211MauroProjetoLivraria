487258cc0b7f657dc43e0886a7519eeb73edf81e
MauroBHJr <abyss.angel@hotmail.com>
2022-07-25 19:07:48 -0300

    blá

--- src/senactechnoite211maurolivraria/SenacTechNoite211MauroLivraria.java
+++ src/senactechnoite211maurolivraria/SenacTechNoite211MauroLivraria.java
@@ -4,6 +4,11 @@
  */
 package senactechnoite211maurolivraria;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import view.menuLivraria;
+
 /**
  *
  * @author 182110041
@@ -15,6 +20,22 @@ public class SenacTechNoite211MauroLivraria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String userAtendente = JOptionPane.showInputDialog(null,"Usuário:","Livraria Lótus - Login",JOptionPane.PLAIN_MESSAGE);
+        
+        
+        
+        if (userAtendente != ""){
+            menuLivraria menuPrincipal = new menuLivraria();
+//            JLabel jlblUserAtendente = new JLabel(userAtendente);
+//            jlblUserAtendente.setVisible(true);
+//            jlblUserAtendente.setSize(200, 100);
+            menuPrincipal.setVisible(true);
+            menuPrincipal.setLocationRelativeTo(null);
+//            menuPrincipal.add(jlblUserAtendente);
+            menuPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } else {
+            System.out.println("Falha ao logar.");
+        }
     }
     
 }
